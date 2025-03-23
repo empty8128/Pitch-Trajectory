494111ee57781d5ac5548f1cd8a42d033e827671
empty8128 <121777700+empty8128@users.noreply.github.com>
2025-03-23 20:55:41 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -21,12 +21,15 @@ def frange(start, end, step):
         list.append(n)
     return list
 
+'''
 def load_data():
-    file_path = 'data/player.json' 
+    file_path = 'data\player.json' 
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 data = load_data()
+'''
+
 
 st.set_page_config(layout="wide")
 
